030ccbeab9fd7c41564792c5bce6d68e7f501013
akram <akramfares2001@gmail.com>
2022-04-14 19:52:06 +0000

    create user-schema

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { NeodeModule } from 'neode-nestjs/dist';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import UserSchema from './dto/user.model';
 
-@Module({})
+@Module({
+    imports:[NeodeModule.forFeature({User: UserSchema})],
+    controllers: [AuthController],
+    providers: [AuthService],
+})
 export class AuthModule {}
