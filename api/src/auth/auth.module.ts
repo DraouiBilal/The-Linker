4744f663e5b8297b314526f43e0d232070cacfda
akram <akramfares2001@gmail.com>
2022-04-14 21:48:06 +0000

    connect successfully to database

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { NeodeModule } from 'neode-nestjs/dist';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import UserSchema from './dto/user.model';
 
 @Module({
-    imports:[NeodeModule.forFeature({User: UserSchema})],
+    imports:[
+        PassportModule.register({defaultStrategy : "jwt" }),
+        JwtModule.register({
+            secret: "jwt-secret",
+            signOptions:{
+                expiresIn: 3600 * 24 * 7 // 7 days 
+            }
+        }),
+        NeodeModule.forFeature({User: UserSchema}),
+    ],
     controllers: [AuthController],
     providers: [AuthService],
 })
