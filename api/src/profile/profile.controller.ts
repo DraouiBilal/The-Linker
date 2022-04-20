cc569c4afe79549e0208927752be0dd298f61970
DraouiBilal <bilaldraoui22@gmail.com>
2022-04-20 23:03:15 +0000

    Finished update profile

--- api/src/profile/profile.controller.ts
+++ api/src/profile/profile.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Put } from '@nestjs/common';
+import { UserInterface } from 'src/auth/interfaces/user.interfaces';
+import { UpdateProfileDto } from './dto/update-profile-dto.dto';
+import { ProfileService } from './profile.service';
 
 @Controller('profile')
-export class ProfileController {}
+export class ProfileController {
+
+    constructor(private profileService: ProfileService){}
+
+    @Put()
+    async updateProfile(@Body() updateProfileDto: UpdateProfileDto) : Promise<UserInterface> {
+        return this.profileService.updateProfile(updateProfileDto)
+    }
+}
--- api/src/profile/profile.service.ts
+++ api/src/profile/profile.service.ts
@@ -1,4 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
+import * as Neode from 'neode'
+import * as bcrypt from 'bcrypt';
+import UserSchema from 'src/auth/dto/user.model';
+import { UserInterface } from 'src/auth/interfaces/user.interfaces';
+import { UpdateProfileDto } from './dto/update-profile-dto.dto';
+import {Neo4jError} from 'neo4j-driver'
 
 @Injectable()
-export class ProfileService {}
+export class ProfileService {
+
+    constructor(
+        @Inject('Connection') private readonly neode: Neode
+    ){
+        neode.with({User:UserSchema})
+    }
+
+    async updateProfile(updateProfileDto: UpdateProfileDto): Promise<UserInterface>{
+        const user:Neode.Node<UserInterface> = await this.neode.first('User','username',updateProfileDto.username)
+        let hashedPassword : string
+
+        try{
+            const salt = await bcrypt.genSalt()
+            hashedPassword = await bcrypt.hash(updateProfileDto.password, salt)
+        }catch{
+            throw new InternalServerErrorException('Server Error')
+        }
+
+        let updatedUser:Neode.Node<UserInterface>
+
+        try{
+            updatedUser = await user.update({
+                ...updateProfileDto,
+                password: hashedPassword,
+                id: user.properties().id,
+                isFirstAuth: user.properties().isFirstAuth
+            })
+            return updatedUser.properties()
+        }catch(err: unknown){
+            if(err instanceof Neo4jError){
+                if((err as Neo4jError).code === 'Neo.ClientError.Schema.ConstraintValidationFailed'){
+                        
+                    const user:Neode.Node<UserInterface> = await this.neode.first('User','email',updateProfileDto.email)
+                    
+                    if(user && !updatedUser)    
+                        throw new ConflictException('Email already exists')
+                }
+            }
+            throw new InternalServerErrorException('Server Error')
+        }
+   }
+}
