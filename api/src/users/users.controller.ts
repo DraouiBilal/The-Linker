9aa66d04dfecd0c9c11773812afbc02462ee53ae
DraouiBilal <bilaldraoui22@gmail.com>
2022-04-22 22:24:20 +0000

    Finished add friend

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Param, Post, UseGuards } from '@nestjs/common';
+import * as Neode from 'neode'
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { UserInterface } from 'src/auth/interfaces/user.interfaces';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -7,4 +10,9 @@ import { UsersService } from './users.service';
 export class UsersController {
 
     constructor(private userService: UsersService){}
+
+    @Post("/:id")
+    async addFriend(@GetUser() userNode:Neode.Node<UserInterface>,@Param('id') id:string): Promise<string>{
+        return await this.userService.addFriend(userNode,id)
+    }
 }
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,6 +1,7 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, InternalServerErrorException } from '@nestjs/common';
 import UserSchema from 'src/auth/dto/user.model';
 import * as Neode from 'neode'
+import { UserInterface } from 'src/auth/interfaces/user.interfaces';
 
 @Injectable()
 export class UsersService {
@@ -8,5 +9,26 @@ export class UsersService {
         @Inject('Connection') private readonly neode: Neode
     ){
         neode.with({User:UserSchema})
+            .model('User')
+            .relationship('friend', 'relationship', 'FRIEND', 'direction_both', 'User')
+    }
+
+    async addFriend(userNode:Neode.Node<UserInterface>, id:string): Promise<string> {
+        let otherUser: Neode.Node<UserInterface>
+
+        try {
+            otherUser = await this.neode.first('User','id',id)
+        } catch (err: unknown) {
+            throw new InternalServerErrorException('Server Error')
+        }
+        try{
+            const relationship = await userNode.relateTo(otherUser,'friend')
+            return relationship.toJson()
+        }catch(err: unknown) {
+            throw new InternalServerErrorException('Server Error')
+        }
+        
     }
 }
+
+
